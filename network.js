0393c678c7a15c98c28fe942c4185608d0e4910e
William Chong <me@williamchong.cloud>
2021-03-30 18:35:32 +0800

    🔧 Add LikeCoin settings

--- network.js
+++ network.js
@@ -1,38 +1,38 @@
 export default {
-  id: 'cosmos-hub-testnet', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
-  name: 'Cosmos Stargate',
+  id: 'likecoin-chain-sheungwan', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
+  name: 'LikeCoin chain',
   description:
-    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
+    'LikeCoin is a Decentralized Publishing Infrastructure for Decentralized Archive, Decentralized Rewards, Decentralized Curation and Decentralized Governance.',
   logo: `logo.svg`,
-  website: 'https://cosmos.network',
-  apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
-  rpcURL: 'ws://34.123.30.100:26657',
-  stakingDenom: 'MUON',
+  website: 'https://like.co',
+  apiURL: 'https://mainnet-node.like.co', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'ws://mainnet-node.like.co:26657',
+  stakingDenom: 'LIKE',
   coinLookup: [
     {
-      viewDenom: 'MUON',
-      chainDenom: 'umuon',
-      chainToViewConversionFactor: 1e-6,
-      icon: `currencies/muon.png`,
+      viewDenom: 'LIKE',
+      chainDenom: 'nanolike',
+      chainToViewConversionFactor: '0.000000001',
+      icon: `currencies/like.png`,
     },
   ],
   addressPrefix: 'cosmos',
   validatorAddressPrefix: 'cosmosvaloper',
   validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
-  lockUpPeriod: `3 days`,
+  lockUpPeriod: `21 days`,
   fees: {
     default: {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'MUON',
-          amount: 0.001,
+          denom: 'LIKE',
+          amount: '0.00000001',
         },
       ],
     },
   },
-  icon: `https://lunie.fra1.digitaloceanspaces.com/network-icons/cosmos.png`,
+  icon: `https://like.co/logo.png`,
 
   // This is only to be used as a developer tool and for testing purposes
   // NEVER ENABLE LOCALSIGNING IN PRODUCTION OR FOR MAINNETS
